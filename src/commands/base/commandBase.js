19fd09b264155865e7bdc0b73b8cf725d5dedf57
Ross Bratton <ross.bratton@yahoo.co.uk>
2021-07-11 23:38:23 +0100

    Updated CommandBase to handle more variables in constructor

--- src/commands/base/commandBase.js
+++ src/commands/base/commandBase.js
@@ -1,14 +1,18 @@
 class CommandBase {
+    constructor(message, command) {
+        this.message = message;
+        this.command = command;
+    }
+
     // Runs the command.
-    run(message, command, commandFunction = null) {
-        
-        if (message.content != `!${command}`)
+    run(commandFunction = null) {
+        if (this.message.content != `!${this.command}`)
             return;
 
         if (commandFunction != null)
             commandFunction();
 
-        console.log(`Successfully ran "!${command}" command.`);
+        console.log(`Successfully ran "!${this.command}" command.`);
     }
 }
 
--- src/commands/fun/ping.js
+++ src/commands/fun/ping.js
@@ -1,12 +1,13 @@
 const CommandBase = require('../base/commandBase');
+const commandName = 'ping';
 
 class PingCommand extends CommandBase {
-  constructor(message) {
-    super();
-    this.run(message, 'ping', () => {
-        message.channel.send('Pong!');
-    });
-  }
+    constructor(message) {
+        super(message, commandName);
+        this.run(() => {
+            this.message.channel.send('Pong!');
+        });
+    }
 }
 
 module.exports = PingCommand;
